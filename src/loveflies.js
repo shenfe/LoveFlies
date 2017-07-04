985f9f64227b812d2b65b5eb04bd3d6cead52d36
hengwu <youchuangqiba@163.com>
2017-07-05 02:37:53 +0800

    add: rotate and scale.

--- src/loveflies.js
+++ src/loveflies.js
@@ -2,7 +2,7 @@
  * @Author: godzilla
  * @Date:   2017-07-05T01:52:56+08:00
  * @Last modified by:   godzilla
- * @Last modified time: 2017-07-05T02:27:42+08:00
+ * @Last modified time: 2017-07-05T02:37:31+08:00
  */
 
 /**
@@ -14,8 +14,9 @@ var creator = function ($container, option) {
     var svgHtml = `
 <svg width="200" height="400" xmlns="http://www.w3.org/2000/svg">
   <text font-size="40" x="0" y="0" fill="#cd0000">❤
-    <animateMotion path="M10,80 q100,120 120,20 q140,-50 160,0" begin="0s" dur="3s" repeatCount="1"/>
-    <animate attributeName="opacity" from="1" to="0" begin="0s" dur="3s" repeatCount="1" />
+    <animateMotion path="M10,80 q100,120 120,20 q140,-50 160,0" rotate="auto" begin="0s" dur="2s" repeatCount="1"/>
+    <animate attributeName="opacity" from="1" to="0" begin="0s" dur="2s" repeatCount="1" />
+    <animateTransform attributeName="transform" begin="0s" dur="2s" type="scale" from="1" to="2" repeatCount="1"/>
   </text>
 </svg>
 `;
